effb5feaec9227fe80b966c1173d8f8bb760bc26
Egorbl <egor.belyakovvvv@gmail.com>
2023-12-24 20:03:59 +0300

    Added streamlit app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # External packages
 import streamlit as st
+from src.ranking import RankingModel
 
 # Setting page layout
 st.set_page_config(
@@ -8,22 +9,51 @@ st.set_page_config(
     layout="centered",
 )
 
-source_radio = st.radio(
+task = st.radio(
     "Выберите задачу", ["Поиск вакансий", "Поиск резюме"])
 
-if source_radio == "Поиск вакансий":
-    vacancy_name = st.text_area(
+model_name = st.radio(
+    "Модель", ["distiluse", "miniLM", "rubert"])
+
+final_text = ""
+task_name = None
+
+if task == "Поиск вакансий":
+    task_name = "vacancy_search"
+    position_name = st.text_area(
         "Желаемая должность", max_chars=60
     )
-    cv_text = st.text_area(
-        "Текст вашего резюме",
-        placeholder="Введите текст своего резюме",
-        height=250)
-elif source_radio == "Поиск резюме":
+    skills = st.text_area(
+        "Релевантные навыки",
+        height=100)
+    work_experience = st.text_area(
+        "Опыт работы",
+        height=100)
+    education = st.text_area(
+        "Образование",
+        height=100)
+    final_text = position_name + " Навыки: " + skills + " Опыт работы: " + work_experience + \
+                 " Образование: " + education
+
+    button_text = 'Найти подходящие вакансии'
+else:
+    task_name = "cv_search"
     vacancy_name = st.text_area(
         "Название вакансии", max_chars=60
     )
-    vacancy_text = st.text_area(
-        "Текст вашей вакансии",
-        placeholder="Введите текст своей вакансии",
-        height=250)
+    position_requirements = st.text_area(
+        "Требования к кандидату",
+        height=100)
+    position_responsibilities = st.text_area(
+        "Обязанности",
+        height=100
+    )
+    final_text = vacancy_name + " Требования: " + position_requirements +\
+                 " Обязанности: " + position_responsibilities
+
+    button_text = 'Найти подходящих кандидатов'
+
+if st.button(button_text):
+    model = RankingModel(10)
+    best_candidates = model.get_relevant_candidates(final_text, model_name, task_name)
+    st.write(best_candidates)
